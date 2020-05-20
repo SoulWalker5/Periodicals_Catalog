cb1ab0126ed8912bc82694acb8b7c9aefb2a2363
soulwalker5 <shrituon@gmail.com>
2020-05-20 13:54:56 +0300

    Added login to site

--- Periodicals_Catalog_MVC/App_Start/IdentityConfig.cs
+++ Periodicals_Catalog_MVC/App_Start/IdentityConfig.cs
@@ -54,7 +54,6 @@ namespace Periodicals_Catalog_MVC
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
